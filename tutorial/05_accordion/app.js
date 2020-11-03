b22663eb3c9318b497fe5e76258cb896d2e4e2bd
harimbu <harimbu@gmail.com>
2020-11-03 10:21:56 +0900

    js

--- tutorial/05_accordion/app.js
+++ tutorial/05_accordion/app.js
@@ -1,2 +1,8 @@
 const content = document.querySelectorAll('.content')
-console.log(content)
+
+for (let i = 0; i < content.length; i++) {
+  content[i].addEventListener('click', function () {
+    console.log(this)
+    this.classList.toggle('active')
+  })
+}
